3705aa6d0404591dadd96dbd67856b8051b0220f
7Yura <svido777@gmail.com>
2022-11-08 16:20:02 +0200

    rick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  title = 'rick_morty_project';
+  title = 'The Rick and Morty';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+// import {RouterLinkWithHref, RouterOutlet} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {CharacterComponent} from './components/character/character.component';
+import {CharactersComponent} from './components/characters/characters.component';
+// import {CharactersHeaderComponent} from './components/characters-header/characters-header.component';
+import {EpisodeComponent} from './components/episode/episode.component';
+import {EpisodesComponent} from './components/episodes/episodes.component';
+// import {EpisodesHeaderComponent} from './components/episodes-header/episodes-header.component';
+import {MainLayoutComponent} from "./layouts";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterModule} from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    CharactersComponent,
+    // CharactersHeaderComponent,
+    EpisodeComponent,
+    EpisodesComponent,
+    // EpisodesHeaderComponent,
+    // MainHeaderComponent,
+    MainLayoutComponent
+
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    // RouterLinkWithHref,
+    // RouterOutlet,
+    MatIconModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
